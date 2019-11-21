629243b77fd0452e029e872531110eb961f945d2
Lucas Amaral <lopesdoamaral@gmail.com>
2019-11-21 17:23:22 -0300

    Using 'visit' to navigate to a given URL

--- spec/test_selenium_capybara.rb
+++ spec/test_selenium_capybara.rb
@@ -1,15 +1,17 @@
 require 'selenium-webdriver'
 
+#rspec --tag status:'ready' .\spec\test_selenium_capybara.rb
+
 describe 'opening website' do
     example 'opening google', :status => 'ready' do
-
+        visit "https://google.com"
     end
 
     example 'interacting with browser elements :id, :tag_name, send_keys', :status => 'not-ready' do
         
     end
 
-    example 'interacting with browser elements :class', :status => 'not-ready' do
-        #http://the-internet.herokuapp.com/login"
+    example 'interacting with browser elements :class', :status => 'ready' do
+        visit "/login"
     end
 end
